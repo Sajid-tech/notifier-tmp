922b922068599812d1f917cfa44eca98a6ed0128
Sajid Hussain <80850448+Sajid-tech@users.noreply.github.com>
2025-05-20 21:32:08 +0530

    Fix CSS build configuration

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { NotifierProvider } from './context/NotifierContext';
 import { Notifier as NotifierComponent } from './components/Notifier';
 import { useNotifier } from './hooks/useNotifier';
 import { createNotifierService } from './services/NotifierService';
+import './style.css';
 
 const notifier = createNotifierService();
 
